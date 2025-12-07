37bcec99794f0026cb96cc6705240c01e2034eb2
Euan Kemp <euank@euank.com>
2025-12-08 01:20:41 +0900

    day07: part2

--- day07/default.nix
+++ day07/default.nix
@@ -13,25 +13,33 @@ let
       start = startIndex;
     };
 
-  part1 =
+  run =
     let
       p = parse input;
-      doSplit = beams: splitters:
+      doSplit = beams: splitters: paths:
         let
           beams' = (builtins.sort builtins.lessThan (lists.unique beams));
           splitters' = builtins.sort builtins.lessThan splitters;
           nextBeam = lists.head beams';
           nextSplitter = lists.head splitters';
+          pathsIfSplit = paths // {
+            "${toString (nextSplitter + 1)}" = (paths."${toString nextSplitter}") + (paths."${toString (nextSplitter + 1)}" or 0);
+            "${toString (nextSplitter - 1)}" = (paths."${toString nextSplitter}") + (paths."${toString (nextSplitter - 1)}" or 0);
+            "${toString nextSplitter}" = 0;
+          };
         in
-        if (lists.length beams) == 0 || (lists.length splitters) == 0 then { beams = beams'; splits = 0; }
-        else if nextBeam == nextSplitter then let s = (doSplit (lists.tail beams') splitters'); in { beams = [ (nextBeam - 1) (nextBeam + 1) ] ++ s.beams; splits = 1 + s.splits; }
-        else if nextBeam < nextSplitter then let s = doSplit (lists.tail beams') splitters; in { beams = [ nextBeam ] ++ s.beams; splits = s.splits; }
-        else let s = doSplit beams' (lists.tail splitters'); in { beams = s.beams; splits = s.splits; };
+        if (lists.length beams) == 0 || (lists.length splitters) == 0 then { paths = paths; beams = beams'; splits = 0; }
+        else if nextBeam == nextSplitter then let s = (doSplit (lists.tail beams') splitters' pathsIfSplit); in { paths = s.paths; beams = [ (nextBeam - 1) (nextBeam + 1) ] ++ s.beams; splits = 1 + s.splits; }
+        else if nextBeam < nextSplitter then let s = doSplit (lists.tail beams') splitters paths; in { paths = s.paths; beams = [ nextBeam ] ++ s.beams; splits = s.splits; }
+        else doSplit beams' (lists.tail splitters') paths;
 
-      endState = builtins.foldl' (state: line: let lineRes = doSplit state.beams line; in { splits = state.splits + lineRes.splits; beams = lineRes.beams; }) { splits = 0; beams = [ p.start ]; } p.splitters;
+      endState = builtins.foldl' (state: line: let lineRes = doSplit state.beams line state.paths; in { paths = lineRes.paths; splits = state.splits + lineRes.splits; beams = lineRes.beams; }) { paths = { "${toString p.start}" = 1; }; splits = 0; beams = [ p.start ]; } p.splitters;
     in
-    endState.splits;
+    endState;
+
+  part1 = run.splits;
+  part2 = lists.sum (builtins.attrValues run.paths);
 in
 {
-  inherit part1;
+  inherit part1 part2;
 }
